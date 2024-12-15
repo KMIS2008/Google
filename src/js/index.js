1d69c65c5f63a41e1dfed7bc3ba6865d0993113a
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-12-15 09:53:09 +0200

    create index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
-const form = document.querySelector('.search-form');
-console.log(form)
+const logoG1=document.querySelector('.g1');
+const logoO1=document.querySelector('.o1');
+const logoO2=document.querySelector('.o2');
+const logoG2=document.querySelector('.g2');
+const logoL=document.querySelector('.l');
+const logoE=document.querySelector('.e');
+const btnOddity=document.querySelector('.oddity');
+
+
+const handleClick=event=>{
+    logoG1.textContent='O';
+    logoO1.textContent='d';
+    logoO2.textContent='d';
+    logoG2.textContent='i';
+    logoL.textContent='t';
+    logoE.textContent='y'
+}
+
+btnOddity.addEventListener("click", handleClick);
+
